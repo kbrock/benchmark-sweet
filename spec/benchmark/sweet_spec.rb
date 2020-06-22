d9fd5f84490f73737eefcc6c0dc9944e159cc8b2
Keenan Brock <keenan@thebrocks.net>
2020-06-22 10:53:04 -0400

    drop failing test (not optimal)

--- spec/benchmark/sweet_spec.rb
+++ spec/benchmark/sweet_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Benchmark::Sweet do
   it "has a version number" do
     expect(Benchmark::Sweet::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
